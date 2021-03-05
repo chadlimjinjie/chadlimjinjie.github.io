0937638eadcd6db3e3c30dde228d58db09c23e2d
Chad Lim Jin Jie <chadlimjinjie@gmail.com>
2021-03-05 16:47:28 +0800

    Updates

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function getGitUser(user) {
-    return axios.get(`https://api.github.com/users/${user}`, {
-        headers: {
-            "Authorization": "token 916b3f2656ae9b3b360fc69ab97ceb4a166dd624"
-        }
-    }).then(resp => resp.data)
-}
